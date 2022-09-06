3224652b5bd48653b8192f538fa6774b8ae81112
Max1O <fcb10max@gmail.com>
2022-09-06 18:25:21 +0500

    .

--- b/index.js
+++ b/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const router = require("./routes");
+const router = require("./src/routes");
 
 const App = express();
 
 App.use(cors());
 App.use(express.json());
 App.use(router);
-App.use("/assets", express.static(__dirname + "/public/audios"))
+App.use("/assets", express.static(__dirname + "/src/public/audios"))
 
 App.listen(5000, function (err) {
   if (err) console.log(err);
