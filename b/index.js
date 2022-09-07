342ececef72b1009017ec2c53a813f9c414fb572
Max1O <fcb10max@gmail.com>
2022-09-07 13:38:52 +0500

    backend update

--- b/index.js
+++ b/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const router = require("./src/routes");
+const sendAllAudios = require("./src/controllers/sendAllAudios");
+const sendSingleAudio = require("./src/controllers/sendSingleAudio");
 
 const App = express();
 
 App.use(cors());
 App.use(express.json());
-App.use(router);
 App.use("/assets", express.static(__dirname + "/src/public/audios"))
+
+
+App.get("/?", sendAllAudios);
+App.get("/audio?", sendSingleAudio);
 
 App.listen(5000, function (err) {
   if (err) console.log(err);
