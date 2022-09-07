e2b79f5d14c57d755ba3c78d1e78fd7a58303669
Max1O <fcb10max@gmail.com>
2022-09-07 11:42:11 +0500

    .

--- b/src/routes/index.js
+++ b/src/routes/index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const cors = require("cors");
+// const cors = require("cors");
 
 const sendAllAudios = require("../controllers/sendAllAudios.js");
 const sendSingleAudio = require("../controllers/sendSingleAudio");
 
-router.use(cors({origin: ["https://music-app-pink.vercel.app", "http://localhost:3000"]}));
+// router.use(cors({origin: ["https://music-app-pink.vercel.app", "http://localhost:3000"]}));
 router.get("/?", sendAllAudios);
 router.get("/audio?", sendSingleAudio);
 
